bd030906b6843ff3337bbc98e918909666f681ca
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2022-08-14 23:00:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function nameFun(){
+    var nameBox = document.querySelector(".main");
+    var addName = document.querySelector("#add-name");
+    var addList = document.querySelector(".add-main");
+    var nameValue = document.getElementById("name").value;
+    localStorage.setItem('name', nameValue);
+    console.log(localStorage.getItem('name'));
+    nameBox.style.visibility = "hidden";
+    nameBox.style.display = "none";
+    addList.style.visibility = "visible";
+    addName.innerHTML = "welcome"+" "+nameValue;
+
+}
+
+function listFun(){
+    var ipVal = document.querySelector("#add-text").value
+    if( ipVal==""){
+        alert("List Input cannot be blank")
+    }
+    else{
+
+    var colors = [
+        '#FF4B4B',
+        '#FF4B7C',
+        '#D34BFF',
+        '#944BFF',
+        '#4B6CFF',
+        '#4BCAFF',
+        '#4BFFA4',
+        '#5DFF4B',
+        '#FFFF4B',
+        '#FFBA4B',
+         ];
+    
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+
+    document.querySelector("#list").innerHTML +=
+    `<div class="list-main" style="color:red;display:inline-block;margin-left:5px;">
+    <button style="text-align:center;background-color:${random_color};color:white;height:auto;text-align:center;
+    border-radius:15px;padding-left:10px;padding-right:10px;padding-top:5px;padding-bottom:5px;border:none;outline:none;" id="list-name">
+    ${document.querySelector("#add-text").value}
+    </button></div>`
+
+    document.querySelector("#add-text").value ="";
+
+  }
+}
+
+
+
+
+
+
+
