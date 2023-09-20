a454e74b338459b401096d6bc4cc793d3a101aee
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2023-09-20 21:03:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function nameFun(){
     var nameBox = document.querySelector(".main");
     var addName = document.querySelector("#add-name");
     var addList = document.querySelector(".add-main");
-    
+    var nameValue = document.getElementById("name").value;
+    localStorage.setItem('name', nameValue);
+    console.log(localStorage.getItem('name'));
+    nameBox.style.visibility = "hidden";
+    nameBox.style.display = "none";
+    addList.style.visibility = "visible";
+    addName.innerHTML = "welcome"+" "+nameValue;
 
 }
 
